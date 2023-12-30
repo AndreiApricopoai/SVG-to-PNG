2159d5348d87bfcb3d74a09b412d0b78fc0fb475
unknown <andreiapricopoai15@gmail.com>
2023-12-30 15:45:46 +0200

    finish draw line, working on draw circle

--- SVG-To-PNG-Converter/converter/svg_png_converter.py
+++ SVG-To-PNG-Converter/converter/svg_png_converter.py
@@ -18,6 +18,19 @@ class SvgPngConverter(Converter):
                 return attribute.value
         return ''
 
+    def __get_float(self, svg_object: SvgDeserializedObject, attribute_name: str, default: float = 0):
+        try:
+            return float(self.__extract_attribute_value(svg_object, attribute_name))
+        except ValueError:
+            return default
+
+    def __get_string(self, svg_object: SvgDeserializedObject, attribute_name: str, default: str = None):
+        value = self.__extract_attribute_value(svg_object, attribute_name)
+        if value == '':
+            return default
+        return value
+
+
     def convert(self):
         for svg_deserialized_object in self.deserialized_objects:
             if svg_deserialized_object.tag_name == 'svg':
@@ -45,27 +58,65 @@ class SvgPngConverter(Converter):
         pass
 
     def __draw_circle(self, circle_object: SvgDeserializedObject):
-        pass
+        cx = self.__get_float(circle_object, 'cx', default=0)
+        cy = self.__get_float(circle_object, 'cy', default=0)
+        r = self.__get_float(circle_object, 'r', default=0)
+
+        stroke = self.__get_string(circle_object, 'stroke', default='black')
+        stroke_width = self.__get_float(circle_object, 'stroke-width', default=1)
+        stroke_opacity = self.__get_float(circle_object, 'stroke-opacity', default=255)
+        fill = self.__get_string(circle_object, 'fill', default='none')
+        fill_opacity = self.__get_float(circle_object, 'fill-opacity', default=255)
+
+        try:
+            # Convert stroke color to RGBA if necessary
+            if stroke_opacity < 255:
+                color_rgb = ImageColor.getcolor(stroke, "RGB")
+                stroke = color_rgb + (stroke_opacity,)
+
+            # Convert fill color to RGBA if necessary
+            if fill_opacity < 255 and fill != 'none':
+                fill_rgb = ImageColor.getcolor(fill, "RGB")
+                fill = fill_rgb + (fill_opacity,)
+            elif fill == 'none':
+                fill = None  # No fill
+
+            # Calculate bounding box for circle
+            left = cx - r
+            top = cy - r
+            right = cx + r
+            bottom = cy + r
+
+            # Draw the circle
+            self.draw.ellipse([left, top, right, bottom], outline=stroke, fill=fill, width=stroke_width)
+
+        except ValueError as ve:
+            print(f"Could not process circle attributes: {ve}")
+        except Exception as e:
+            print(f"Could not draw the circle: {e}")
 
     def __draw_ellipse(self, ellipse_object: SvgDeserializedObject):
         pass
 
     def __draw_line(self, line_object: SvgDeserializedObject):
-        x1 = float(self.__extract_attribute_value(line_object, 'x1'))
-        y1 = float(self.__extract_attribute_value(line_object, 'y1'))
-        x2 = float(self.__extract_attribute_value(line_object, 'x2'))
-        y2 = float(self.__extract_attribute_value(line_object, 'y2'))
+        x1 = self.__get_float(line_object, 'x1', default=0)
+        y1 = self.__get_float(line_object, 'y1', default=0)
+        x2 = self.__get_float(line_object, 'x2', default=0)
+        y2 = self.__get_float(line_object, 'y2', default=0)
 
-        stroke = self.__extract_attribute_value(line_object, 'stroke')
-        stroke_width = float(self.__extract_attribute_value(line_object, 'stroke-width'))
-        stroke_opacity = float(self.__extract_attribute_value(line_object, 'stroke-opacity'))
-
-        if stroke_opacity < 255:
-            color_rgb = ImageColor.getcolor(stroke, "RGB")
-            stroke = color_rgb + (stroke_opacity,)
+        stroke = self.__get_string(line_object, 'stroke', default='black')
+        stroke_width = self.__get_float(line_object, 'stroke-width', default=1)
+        stroke_opacity = self.__get_float(line_object, 'stroke-opacity', default=255)
 
         try:
+            if stroke_opacity < 255:
+                color_rgb = ImageColor.getcolor(stroke, "RGB")
+                stroke = color_rgb + (stroke_opacity,)
+
             self.draw.line((x1, y1, x2, y2), fill=stroke, width=stroke_width)
+
+        except ValueError as ve:
+            print(f"Could not process stroke attributes: {ve}")
         except Exception as e:
             print(f"Could not draw the line: {e}")
 
--- SVG-To-PNG-Converter/main.py
+++ SVG-To-PNG-Converter/main.py
@@ -1,8 +1,8 @@
 from converter.svg_deserialization import SvgDeserializer
 from converter.svg_png_converter import SvgPngConverter
 
-deserializer = SvgDeserializer('file.svg')
-deserialized_elements = deserializer.deserialize()
-path = 'C:\\Users\\Andrei\\Desktop\\SVG-to-PNG\\SVG-To-PNG-Converter\\imagine.png'
-converter = SvgPngConverter(deserialized_elements, (500, 500), path)
-converter.convert()
+#deserializer = SvgDeserializer('file.svg')
+#deserialized_elements = deserializer.deserialize()
+#path = 'C:\\Users\\Andrei\\Desktop\\SVG-to-PNG\\SVG-To-PNG-Converter\\imagine.png'
+#converter = SvgPngConverter(deserialized_elements, (500, 500), path)
+#converter.convert()
