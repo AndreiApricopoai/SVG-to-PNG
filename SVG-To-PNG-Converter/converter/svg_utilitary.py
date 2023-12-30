05c5e8d426a8d7294e56ed91b9afed12b1c09ab1
unknown <andreiapricopoai15@gmail.com>
2023-12-30 22:29:46 +0200

    Refactor SvgUtility static methods

--- SVG-To-PNG-Converter/converter/svg_utilitary.py
+++ SVG-To-PNG-Converter/converter/svg_utilitary.py
@@ -3,29 +3,29 @@ from converter.svg_deserialization import SvgDeserializedObject
 
 class SvgUtility:
     @staticmethod
-    def extract_attribute_value(self, svg_object: SvgDeserializedObject, attribute_name: str) -> str:
+    def __extract_attribute_value(svg_object: SvgDeserializedObject, attribute_name: str) -> str:
         for attribute in svg_object.attributes:
             if attribute.name == attribute_name:
                 return attribute.value
         return ''
 
     @staticmethod
-    def get_float(self, svg_object: SvgDeserializedObject, attribute_name: str, default: float = 0):
+    def get_float(svg_object: SvgDeserializedObject, attribute_name: str, default: float = 0):
         try:
-            return float(self.extract_attribute_value(svg_object, attribute_name))
+            return float(SvgUtility.__extract_attribute_value(svg_object, attribute_name))
         except ValueError:
             return default
 
     @staticmethod
-    def get_string(self, svg_object: SvgDeserializedObject, attribute_name: str, default: str = None):
-        value = self.extract_attribute_value(svg_object, attribute_name)
+    def get_string(svg_object: SvgDeserializedObject, attribute_name: str, default: str = None):
+        value = SvgUtility.__extract_attribute_value(svg_object, attribute_name)
         if value == '':
             return default
         return value
 
     @staticmethod
-    def get_int(self, svg_object: SvgDeserializedObject, attribute_name: str, default: int = 0):
+    def get_int(svg_object: SvgDeserializedObject, attribute_name: str, default: int = 0):
         try:
-            return int(self.extract_attribute_value(svg_object, attribute_name))
+            return int(SvgUtility.__extract_attribute_value(svg_object, attribute_name))
         except ValueError:
             return default
